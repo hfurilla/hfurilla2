8e15e2e68a3ba050642fc5b276ca92b153929dfe
hfurilla <hailey.furilla@gmail.com>
2025-05-15 19:54:19 -0400

    Add files via upload

--- HTML5/Data/Search.js
+++ HTML5/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Accessibility/Design-differences.htm'],s:['1'],p:[['1',1],['preview',5]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Accessibility/Design-differences.htm'],s:['0'],p:[['0',7],['implementing',2],['too',4]]});
